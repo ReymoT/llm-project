cbdf05dccf4b7a3c73450d98c3a8b97dd945bce9
Reimo Tamme <tamme.reimo@gmail.com>
2026-05-31 02:59:10 -0700

    Fixed compatibility issues

--- gateway/app/config.py
+++ gateway/app/config.py
@@ -2,9 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file = ".env", env_file_encoding = "utf-8")
-    gateway_api_key: str
+    gateway_api_key: str = "openserve-key"
     vllm_base_url: str = "http://localhost:8001"
-    vllm_api_key: str
+    vllm_api_key: str = "dev-vllm-key"
     request_timeout_seconds: int = 60
 
 settings = Settings()
